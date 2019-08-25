c59ee9b20587193d3e0632edc358705f22a65f4b
Dirk Janssen <dirk.janssen@inrix.com>
2019-08-25 16:03:52 -0700

    New main routine that plots the same four flowers each time

--- turtle_flower.py
+++ turtle_flower.py
@@ -50,8 +50,21 @@ def make_flower(x, y, petals, petal_color, center_color):
     ))
 
 
-
 def main():
+    """
+    First ran 'main_old' (below) a few times and clicked randomly.
+    This looked nice and I captured it from the print messages
+    I rounded the color numbers so it is easier to read.
+    """
+    make_flower(x=112.0, y=49.0, petals=9, petal_color=[0.691, 0.952, 0.933], center_color=[0.474, 0.135, 0.776])
+    make_flower(x=-68.0, y=-95.0, petals=7, petal_color=[0.181, 0.563, 0.706], center_color=[0.614, 0.701, 0.063])
+    make_flower(x=29.0, y=-26.0, petals=5, petal_color=[0.471, 0.115, 0.637], center_color=[0.639, 0.072, 0.988])
+    make_flower(x=-64.0, y=34.0, petals=6, petal_color=[0.173, 0.823, 0.970], center_color=[0.290, 0.376, 0.607])
+    # need to call this or the screen will go away immediately
+    turtle.done()
+
+
+def main_old():
     ""
     window.onclick(make_flower_get_position)
     window.listen()
